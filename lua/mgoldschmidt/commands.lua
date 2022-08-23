9a9b1cb0f8051c5280de0a0c94cd040c5170d0a6
Maximilian Goldschmidt <mgoldschmidt@microsoft.com>
2022-08-23 13:29:41 +0100

    add debugger

--- lua/mgoldschmidt/commands.lua
+++ lua/mgoldschmidt/commands.lua
@@ -1,7 +1,5 @@
+-- Jester
 vim.api.nvim_create_user_command("JestCurrent", ":lua require'jester'.run_file()", { nargs = 0 })
-
 vim.api.nvim_create_user_command("JestLast", ":lua require'jester'.run_last()", { nargs = 0 })
-
 vim.api.nvim_create_user_command("DebugCurrent", ":lua require'jester'.debug_file()", { nargs = 0 })
-
 vim.api.nvim_create_user_command("DebugLast", ":lua require'jester'.debug_last()", { nargs = 0 })
